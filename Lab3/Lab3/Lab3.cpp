97ad46355772ad80e22e936a7493c0c873fb5242
sobol-mo <55525227+sobol-mo@users.noreply.github.com>
2020-10-30 17:39:38 +0200

    Update

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -5,7 +5,7 @@
 /**************************************************/
 #define  _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include "windows.h";
+#include "windows.h"
 using namespace std;
 
 int main(void) {
